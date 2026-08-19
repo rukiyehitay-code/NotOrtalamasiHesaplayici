0f7dd73e0f745ed251759f2bfb69b1754822d885
ASUS <ASUS@LAPTOP-A947ROO3>
2026-08-19 15:16:01 +0300

    Proje dosyası ekle.

---
+++ NotOrtalamasıHesaplayıcı/Program.cs
@@ -0,0 +1,82 @@
+﻿Console.Write("Kaç Ders Girmek İstiyorsunuz: ");
+int dersSayisi = Convert.ToInt32(Console.ReadLine());
+
+string[] dersler = new string[dersSayisi];
+double[] ortalamalar = new double[dersSayisi];
+string[] harfNotlari = new string[dersSayisi];
+double toplam = 0;
+
+for (int i =0; i < dersSayisi; i++)
+{
+    Console.Write($"{i+1}. Dersin Adını Girin: ");
+    string dersAdi= Console.ReadLine();
+    dersler[i] = dersAdi;
+
+    Console.Write("Vize Notunuzu Girin: ");
+    int vizeNotu = Convert.ToInt32(Console.ReadLine());
+
+    Console.Write("Final Notunuzu Girin: ");
+    int finalNotu = Convert.ToInt32(Console.ReadLine());
+
+    double ortalama = (vizeNotu * 0.40) + (finalNotu * 0.60);
+    ortalamalar[i] = ortalama;
+
+    string harfNotu = "";
+
+    if (ortalama >= 90)
+    {
+        harfNotu = "AA";
+    }
+    else if (ortalama >= 85)
+    {
+        harfNotu = "BA";
+    }
+    else if (ortalama >= 80)
+    {
+        harfNotu = "BB";
+    }
+    else if (ortalama >= 75)
+    {
+        harfNotu = "CB";
+    }
+    else if (ortalama >= 70)
+    {
+        harfNotu = "CC";
+    }
+    else if (ortalama >= 65)
+    {
+        harfNotu = "DC";
+    }
+    else if (ortalama >= 60)
+    {
+        harfNotu = "DD";
+    }
+    else
+    {
+        harfNotu = "FF";
+    }
+    harfNotlari[i] = harfNotu;
+}
+for (int i = 0; i < dersSayisi; i++)
+{
+    toplam = toplam + ortalamalar[i];
+}
+
+double gno = toplam / dersSayisi;
+Console.WriteLine("Genel Not Ortalamanız: " + gno);
+
+for(int i =0; i < dersSayisi; i++)
+{
+    Console.WriteLine("Ders : " + dersler[i]);
+    Console.WriteLine("Ortalama : " + ortalamalar[i]);
+    Console.WriteLine("HarfNotu : " + harfNotlari[i]);
+
+    if (harfNotlari[i] == "FF")
+    {
+        Console.WriteLine("Kaldınız.");
+    }
+    else
+    {
+        Console.WriteLine("Geçtiniz.");
+    }
+}
